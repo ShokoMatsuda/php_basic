43c469c727f66a8e8285b00294a328ac15c5fe11
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-25 17:26:57 +0900

    初回コミット

---
+++ php-basic/variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 変数に値を代入する
+        $user_name = '侍太郎';
+
+        // 変数の中身を出力する
+        echo $user_name;
+        ?>
+    </p>
+</body>
+</html>
