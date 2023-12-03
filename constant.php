53bd256a514d96e9aff966ebe2113f50ed25eec6
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-03 16:38:39 +0900

    17.6 定数に配列を入れてみよう

--- constant.php
+++ constant.php
@@ -12,12 +12,35 @@
 
         // 定数の中身を出力する
         echo SHIPPING_FEE;
+        ?>
+    </p>
+    <p>
+        <?php
+        // クラスを定義する
+        class Product {
+            // クラス内に定数を定義する
+            public const TAX = 1.1;
+        }
 
-        // 定数に値を再代入する（エラー）
-        const SHIPPING_FEE = 800;
+        // クラス内に定義された定数にアクセスする
+        echo Product::TAX;
+        ?>
+    </p>
+    <p>
+        <?php
+        // 定数に配列を入れる
+        const SEVEN_REGIOMS = [
+            '北海道地方',
+            '東北地方',
+            '関東地方',
+            '中部地方',
+            '近畿地方',
+            '中国・四国地方',
+            '九州地方',
+        ];
 
-        // 定数の中身を出力する
-        echo SHIPPING_FEE;
+        // 定数（配列）の中身を出力する
+        print_r(SEVEN_REGIOMS);
         ?>
     </p>
 </body>
