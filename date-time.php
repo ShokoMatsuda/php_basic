7048b54d6cae6d35d58211d32964dd77caf6c42c
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-06 22:20:45 +0900

    2つの日時の差を比較してみよう

--- date-time.php
+++ date-time.php
@@ -20,5 +20,23 @@
         echo date('Y/m/d H:i:s', strtotime('-3 year'));
         ?>
     </p>
+    <p>
+        <?php
+        // 指定した日時のインスタンスを作成する
+        $date_time = new DateTime('2015-03-19 12:15:30');
+
+        // インスタンス$date_timeの日時を特定のフォーマットで出力する
+        echo $date_time->format('Y年n月j日H時i分s秒') . '<br>';
+
+        // 現在の日時のインスタンスを作成する
+        $now = new DateTime();
+
+        // diff()メソッドで2つのインスタンスの日時の差を取得し、変数$intervalに代入する（interval＝間隔）
+        $interval = $now->diff($date_time);
+
+        // 取得した日時の差を特定のフォーマットで出力する
+        echo $interval->format('%y年と%m月と%d日の差があります。総日数は%a日です。') . '<br>';
+        ?>
+    </p>
 </body>
 </html>
