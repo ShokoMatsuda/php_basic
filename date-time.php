c6125984874488ab8ba2ead72c5a9d2a244d7dd5
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-06 22:07:40 +0900

    date()関数とstrtotime()関数を組み合わせて使ってみよう

--- date-time.php
+++ date-time.php
@@ -12,6 +12,12 @@
 
         // 現在の日時を指定したフォーマットで出力する
         echo date('Y年n月j日H時i分s秒') . '<br>';
+
+        // 現在を基準として1週間後の日時のUNIXタイムスタンプを出力する
+        echo strtotime('+1 week') . '<br>';
+
+        // 現在を基準として3年前の日時を指定したフォーマットで出力する
+        echo date('Y/m/d H:i:s', strtotime('-3 year'));
         ?>
     </p>
 </body>
