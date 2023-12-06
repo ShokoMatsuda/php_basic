ff824a8c4b7f47283241d9dfd4a8ccf8b0187b3c
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-06 22:55:25 +0900

    初回コミット

---
+++ error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 1を0で割れないためエラーとなる
+        $test = 1 / 0;
+        ?>
+    </p>
+</body>
+</html>
